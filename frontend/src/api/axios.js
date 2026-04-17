b0ef1d683cd5e0015dd060f1ee58b73f44b4e058
purab04 <purabawasthi004@gmail.com>
2026-04-17 17:12:14 +0530

    Updated backend API URL to Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://e-comm-full-n8c0.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
